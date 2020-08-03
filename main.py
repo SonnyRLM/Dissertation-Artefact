93554b6fa8c2b0c0e43467e69d4dc54173e8dbd0
Sonny <sonnymil123@gmail.com>
2020-08-03 13:19:08 +0100

    Added ability to select image not included in eval_data

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.image as mpimg
 import torchvision.transforms as transforms
 import sys
 from PyQt5 import QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from gui import Ui_MainWindow
 import ntpath
 
@@ -24,16 +24,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def select_image_file(self):
         # Opens the file picker and sets the returned path to a variable.
-        image_file = QFileDialog.getOpenFileName(self, 'Single File', 'data/eval_images')
+        self.image_file = QFileDialog.getOpenFileName(self, 'Single File', 'data/eval_images')
 
         # Updates the pixmap with the selected image.
-        self.image_pixmap.setPixmap(QtGui.QPixmap(str(image_file[0])))
+        self.image_pixmap.setPixmap(QtGui.QPixmap(str(self.image_file[0])))
 
         # Updates the label with the file path.
-        self.image_path_label.setText(str(image_file[0]))
+        self.image_path_label.setText(str(self.image_file[0]))
 
         # Removing the OS file path leaving on the filename and extension.
-        image_file = ntpath.basename(image_file[0])
+        image_file = ntpath.basename(self.image_file[0])
 
         # Removes the file extensions.
         self.image_num = image_file[0:-4]
@@ -45,42 +45,57 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def estimate_boneage(self):
         # Checks if the image_num has been set if so the function continues if not a alert is shown.
-        if self.image_num is 0:
+        if not self.image_num: # If no image selected
             alert = QtWidgets.QMessageBox()
             alert.setWindowTitle("Image Selection Error!")
             alert.setText("Please select an image!")
             alert.setIcon(QtWidgets.QMessageBox.Critical)
             alert.exec_()
         else:
+            input_image = self.image_num
 
-            input_image = int(self.image_num)
+
 
             # Disabling the predict button while the the images are being processed.
             self.predict_button.setEnabled(False)
 
             model_path = 'model/model.pth'
-            images_path = 'data/eval_images/'
-
-            img_name = os.path.join(images_path, str(input_image) + '.png')
+            image_path = self.image_file[0]
 
             # Calculate mean and standard deviation to convert z-score back into months
             train_df = pd.read_csv('data/eval_data.csv', index_col='id')
-
             mean_boneage = train_df['boneage'].mean()
             std_boneage = train_df['boneage'].std()
 
-            # Extract image data from evaluation set
-            img_data = train_df.loc[input_image]
+            # If an image is chosen that does not have stored labels, prompt user to input gender
+            try:
+                int(input_image)
+                img_data = train_df.loc[int(input_image)]  # Extract image data from evaluation set
+            except:
+                # Defining alert box to prompt gender input
+                gender_message = QMessageBox()
+                gender_message.setWindowTitle("Please select gender")
+                gender_message.setText("Is the patient male?")
+                gender_message.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+
+                img_data = train_df.iloc[1].copy()  # Grab a row from dataset for editing
+                img_data['bone_age_z'] = 100  # Using this value to indicate unknown age
+
+                retval = gender_message.exec_()  # Display alert box and take user input
+                if retval == 16384:
+                    img_data['gender'] = 1
+                else:
+                    img_data['gender'] = 0
 
             # Declaring preprocessing transforms
             preprocess_transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
 
-            image = mpimg.imread(img_name)  # Read image
+            image = mpimg.imread(image_path)  # Read image
+
             image = Image.fromarray(image * 255).convert('RGB')  # Convert to RGB
             image = ImageOps.equalize(image, mask=None)  # Equalise image histogram
             image = preprocess_transform(image)  # Apply transforms
             image = image.unsqueeze(0)  # Reformat image
-
             # Use GPU for processing if available
             device = (torch.device("cuda") if torch.cuda.is_available() else "cpu")
 
@@ -88,14 +103,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
             # Move variables to GPU
             image = image.to(device)
-            age = torch.tensor(img_data['bone_age_z']).type(torch.float32).to(device)
             gender = torch.tensor(img_data['gender']).unsqueeze(0).to(device)
+
+            # Age ground truth - if available
+            age = img_data['bone_age_z']
 
             with torch.no_grad():
                 estimated_age = my_model(image, gender)  # Perform inference
 
             # Formatting resultant values in order to be shown in the appropriate TextBrowsers.
-            actual_age = int((std_boneage * age.cpu().detach().item() + mean_boneage))
+            actual_age = int(std_boneage * age + mean_boneage)
             if gender.item() == 1:
                 actual_gender = "Male"
             else:
@@ -103,7 +120,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             predicted_age = int((std_boneage * estimated_age.cpu().detach().item() + mean_boneage))
 
             # Setting the Text Browsers values to the formatted results.
-            self.actual_age_textBrowser.setText(str(actual_age) + " Months")
+            if not img_data['bone_age_z'] == 100:
+                self.actual_age_textBrowser.setText(str(actual_age) + " Months")
+            else:
+                self.actual_age_textBrowser.setText("Unknown")
             self.gender_textBrowser.setText(str(actual_gender))
             self.predicted_age_textBrowser.setText(str(predicted_age) + " Months")
 
